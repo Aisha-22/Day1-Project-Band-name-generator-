7b8f196155b8e2c4c50b4da40d42283b9ff24550
Aishia <51677946+Aisha-22@users.noreply.github.com>
2021-08-24 22:37:10 +0000

    code fix

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ greeting = "Welcome to my First Python Program"
 print(greeting)
 
 #2. Ask the user for the city that they grew up in.
-city_name = input("What is the city you grew up in? ")
+city_name = input("What is the city you grew up in?\n ")
 
 #3. Ask the user for the name of a pet.
-pet_name = input("What is your pet name? ")
+pet_name = input("What is your pet name?\n ")
 
 #4. Combine the name of their city and pet and show them their band name.
 print("Your band name could be" + " " + city_name + " " + pet_name)
